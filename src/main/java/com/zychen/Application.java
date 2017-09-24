7fa89640330eb6ae9141cca9b5758649dbbdb25a
章源辰 <zhangyuanchen143@163.com>
2017-09-24 23:19:08 +0800

    服务注册中心demo

---
+++ src/main/java/com/zychen/Application.java
@@ -0,0 +1,18 @@
+package com.zychen;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 章源辰
+ * @time: 2017/9/24 15:56
+ * @describion:
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
